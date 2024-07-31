5f064b487de50a2464204569e997f162ca74262c
Thalisson Vieira <32040335+Thalinhos@users.noreply.github.com>
2024-07-31 16:52:04 -0300

    Update express.js.ts
    make callbacks prettier

--- jwt-hono-and-express/express.js.ts
+++ jwt-hono-and-express/express.js.ts
@@ -10,22 +10,25 @@ const secret = 'uih4iu123gh789eyh12u3dbh-73t-17g9u3bwd1734uibjw'
 
 app.post('/getToken', function (req, res) {
     const { name }  = req.body
-    const token = jwt.sign({name: name}, secret, { expiresIn: '3600s' })
-  res.json({token: token, name: name})
+    jwt.sign({name: name}, secret, { expiresIn: '3600s' }, (err, data) => {
+        if (err){
+            return res.json({message: err.message})
+        }
+        res.json({token: data, name: name})
+    })
 })
 
 app.post('/verifyToken', (req, res) => {
-    let verifiedToken = ''
     const { token } = req.body
     jwt.verify(token, secret, (err, data) => {
         if (err) {
-            console.log(err)
+            return res.json({data: err.message})
         }
-        verifiedToken = data
+        return res.json({data: data})
     })
-    return res.json({data: verifiedToken})
 })
 
 app.listen(3000, () => {
     console.log('http://localhost:3000/')
+})
 })
