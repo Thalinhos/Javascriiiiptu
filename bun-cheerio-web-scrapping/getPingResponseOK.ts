d9ddf45d6d9fbc772a04bb4bebf8bcc7130c9eb3
Thalisson Vieira <32040335+Thalinhos@users.noreply.github.com>
2024-08-02 12:40:14 -0300

    pegar ips de maquina em um range x usando nodejs

---
+++ bun-cheerio-web-scrapping/getPingResponseOK.ts
@@ -0,0 +1,28 @@
+const { exec } = require('child_process')
+const  fs  = require('fs')
+
+const ipAddressBase = '10.244.36.'
+const getPing = `ping ${ipAddressBase}`
+
+for(let i = 0; i < 255; i++){
+    exec(`powershell.exe ${getPing}${i}`, (err, stdout, stderr) => {
+        if (err){
+            return
+        }
+        if(stderr){
+            return
+        }
+        if(stdout){
+            if(stdout[82] == 3 && stdout[83] == 2){
+                fs.appendFile('pings.txt', `${ipAddressBase}${i} - OK` + '\n', (err) => {
+                    if(err){
+                        console.log(err)
+                    }
+                })
+            }
+        }
+    })
+}
+
+
+
